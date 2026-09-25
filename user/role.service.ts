1ad02cb3a0c02ead17056ad5ed7ce7b2303d2068
Jonsonrath <jonathan.rath@gmx.at>
2026-09-25 09:17:15 +0200

    2026-09-25 09:17:15

--- user/role.service.ts
+++ user/role.service.ts
@@ -1,4 +1,36 @@
 export interface Role {
-    titel: string
+    id: number
+    title: string
 
 }
+
+
+
+const roles: Role[] = [
+    {
+        id: 1,
+        title: "user"
+    },
+    {
+        id: 2,
+        title: "generic"
+    },
+    {
+        id: 3,
+        title: "admin"
+    }
+]
+
+
+
+const getAllRoles = () => {
+    return roles
+}
+const getRole = (id: number) => {
+
+    const role = roles.find((r)=> r.id == id)
+
+    return role
+}
+
+export {getAllRoles, getRole}
--- user/user.service.ts
+++ user/user.service.ts
@@ -1,18 +1,98 @@
 import type { Role } from "./role.service.ts"
 import {v4 as uid} from 'uuid'
 
+import { getAllRoles, getRole} from './role.service.ts'
+
 interface User{
-    id: string
+    id?: string
     name : string
-    roleId: string
-    role: Role
+    roleId: number
+    role?: Role
 }
 
 
 
-const users: User[] = [
+let users: User[] = [
     {
         id: uid(),
-        name: "Mad Max"
-    }
+        name: "Mad Max",
+        roleId: 1
+    },
+    {
+        id: uid(),
+        name: "Sane Seline",
+        roleId: 1
+    },
+    {
+        id: uid(),
+        name: "Generic Gerald",
+        roleId: 2
+    },
+    {
+        id: uid(),
+        name: "Admin Axl",
+        roleId: 3
+    },
 ]
+
+const fetchRoles = () => {
+    users.forEach(u => {
+            u.role = getRole(u.roleId);
+        });
+}
+
+const getAllUsers = () => {
+
+    fetchRoles()
+
+    return users
+}
+
+const getUser = (id: string) => {
+
+    fetchRoles()
+
+    const u = getAllUsers().find(u => u.id === id)
+
+    if(u == undefined){
+        return null
+    }
+
+    u.role = getRole(u.roleId)
+    return u;
+}
+
+const createUser = (user: User) => {
+    user.id = uid();
+    users = [...users, user]
+    fetchRoles()
+    return getAllUsers()
+
+}
+
+const updateUser = (user: User) => {
+
+    const index = getAllUsers().findIndex((u) => u.id === user.id)
+
+    users[index] = user
+
+    fetchRoles();
+    return getAllUsers();
+}
+
+const deleteUser = (id: String) => {
+
+    const index = users.findIndex(u => u.id === id)
+
+    if(index < 0){
+        return
+    }
+
+    users.splice(index, 1)
+    
+    fetchRoles();
+    return getAllUsers();
+}
+
+
+export {getAllUsers, getUser, createUser, updateUser, deleteUser}
